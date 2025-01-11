aa9b61bfe876048ded233f635e38bc0f0f48cd01
prathyanthDhanus <prathyanthbusinessmail@gmail.com>
2025-01-11 13:27:07 +0530

    fix: fixed the app file conflict

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-const express =  require("express");
-const app = express.Router();
+const express = require("express");
+const app = express();
 const cors = require("cors");
+const errorHandler = require("./src/middlewares/errorHandler");
+const auth = require("./src/app/auth/route");
 
 //Middlewares
 app.use(express.json());
 app.use(cors());
 
 
+//Router middleware
+app.use("/api/auth",auth);
 
 
 
-
-
+// Error handling middleware
+app.use(errorHandler);
 
 module.exports = app;
